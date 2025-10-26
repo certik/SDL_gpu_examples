b9db911861246d08b068c9ea64b011a37abcf931
Ondřej Čertík <ondrej@certik.us>
2025-10-26 12:55:25 -0600

    Add a build script

---
+++ b.sh
@@ -0,0 +1,6 @@
+clang MouseCircle_standalone.c -o MouseCircle_standalone \
+    -I$CONDA_PREFIX/include \
+    -L$CONDA_PREFIX/lib \
+    -lSDL3 \
+    -Wl,-rpath,$CONDA_PREFIX/lib \
+    -framework Metal -framework CoreGraphics -framework AppKit

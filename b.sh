1837d7a66d9f42d691a3e5557ac8e469a6a87923
Ondřej Čertík <ondrej@certik.us>
2025-10-26 12:55:56 -0600

    Run it

--- b.sh
+++ b.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+set -ex
+
 clang MouseCircle_standalone.c -o MouseCircle_standalone \
     -I$CONDA_PREFIX/include \
     -L$CONDA_PREFIX/lib \
     -lSDL3 \
     -Wl,-rpath,$CONDA_PREFIX/lib \
     -framework Metal -framework CoreGraphics -framework AppKit
+
+./MouseCircle_standalone
